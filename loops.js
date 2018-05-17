587942dab91d3622d78dfbf2e477462c4608ddd8
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-17 13:33:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
-  n = length.array+1
+  n = []
   for(i = 0; i<25; i++){
     if(i===1) { 
-      array[n+i] = "I am 1 strange loop."
+      array[i] = "I am 1 strange loop."
     }
     else {
-      array[n+i] = `I am ${i} strange loops.`
+      n[i] = `I am ${i} strange loops.`
     }
   }
-  return array
+  return array = [array, ...n]
 }
