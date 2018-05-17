2efccb00d0b55bf130d572fed165da2ba866934c
Michael Iasiello <maiasiel@ncsu.edu>
2018-05-17 13:55:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,5 +21,11 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
+  do {
+    array.pop()
+    return array
+  } while (array.length > 0 && maybeTrue())
 }
